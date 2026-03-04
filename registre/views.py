dc2eedb5cc0bd52d9b15243b102cb8b52936eb2f
Mggmv <mgbehe14@gmail.com>
2026-03-04 19:54:33 +0530

    Update views.py

--- registre/views.py
+++ registre/views.py
@@ -1,30 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.apps import apps
+from .models import ActeNaissance # Nom exact corrigé ici
 
 @login_required
 def dashboard(request):
-    try:
-        # On tente de récupérer le modèle Naissance
-        Naissance = apps.get_model('registre', 'Naissance')
-        derniers_actes = Naissance.objects.all().order_by('-id')[:10]
-    except (LookupError, Exception):
-        # Si le modèle n'existe pas encore, on renvoie une liste vide
-        derniers_actes = []
-    
+    # On récupère les 10 derniers actes de naissance
+    derniers_actes = ActeNaissance.objects.all().order_by('-id')[:10]
     return render(request, 'registre/dashboard.html', {'actes': derniers_actes})
 
 def home(request):
-    # Redirige vers le dashboard si connecté, sinon vers le login
     if request.user.is_authenticated:
         return redirect('dashboard')
     return redirect('login')
 
 @login_required
 def voir_extrait(request, pk):
-    try:
-        Naissance = apps.get_model('registre', 'Naissance')
-        acte = get_object_or_404(Naissance, pk=pk)
-        return render(request, 'registre/extrait_naissance.html', {'acte': acte})
-    except:
-        return redirect('dashboard')
+    # On récupère les données de l'acte de naissance
+    acte = get_object_or_404(ActeNaissance, pk=pk)
+    return render(request, 'registre/extrait_naissance.html', {'acte': acte})
