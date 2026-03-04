fce6ea0d10751f6bb32d93e5c46dddbd96fc2a3f
Mggmv <mgbehe14@gmail.com>
2026-03-04 15:28:20 +0530

    Update urls.py

--- mon_projet/urls.py
+++ mon_projet/urls.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import get_user_model
 from registre.views import apercu_extrait # Import direct pour éviter la boucle
+
+# --- CODE DE SECOURS POUR CRÉER VOTRE COMPTE ---
+try:
+    User = get_user_model()
+    if not User.objects.filter(username='Mggmv').exists():
+        User.objects.create_superuser('Mggmv', 'mgbehe14@gmail.com', 'Gbehe2804')
+except Exception:
+    pass
+# ----------------------------------------------
 
 urlpatterns = [
     # Accès à l'administration
