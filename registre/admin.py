e8426b18adeddd3f5acc480c056730420102b779
Mggmv <mgbehe14@gmail.com>
2026-03-04 21:51:26 +0530

    Update admin.py

--- registre/admin.py
+++ registre/admin.py
@@ -9,15 +9,27 @@ class StructureAdmin(admin.ModelAdmin):
 
 @admin.register(ActeNaissance)
 class ActeNaissanceAdmin(admin.ModelAdmin):
-    # Affichage des colonnes dans la liste des actes
-    list_display = ('nom_complet_officiel', 'date_naissance', 'lieu_naissance', 'bouton_imprimer')
+    # Optimisation : on ne garde que l'essentiel pour l'affichage rapide
+    list_display = ('id_affiche', 'nom_enfant', 'prenoms_enfant', 'date_declaration', 'bouton_imprimer')
+    
+    # Ajout de la pagination pour ne pas charger trop d'actes d'un coup
+    list_per_page = 20 
+    
+    # Recherche rapide
     search_fields = ('nom_enfant', 'prenoms_enfant')
-    list_filter = ('date_naissance', 'structure')
+
+    def id_affiche(self, obj):
+        # Format demandé : 01 du 12/01/2014
+        return f"{obj.id} du {obj.date_declaration.strftime('%d/%m/%Y')}"
+    id_affiche.short_description = 'N° Acte'
 
     def bouton_imprimer(self, obj):
-        # On remplace 'apercu_extrait' par 'voir_extrait' pour corriger l'erreur NoReverseMatch
+        # On utilise voir_extrait car apercu_extrait cause une erreur 404
         url = reverse('voir_extrait', args=[obj.pk])
-        return format_html('<a class="button" href="{}" target="_blank" style="background-color: #28a745; color: white;">Imprimer Extrait</a>', url)
-
-    bouton_imprimer.short_description = 'Action'
-    bouton_imprimer.allow_tags = True
+        return format_html(
+            '<a class="button" href="{}" target="_blank" '
+            'style="background-color: #28a745; color: white; padding: 5px 10px; border-radius: 4px;">'
+            'Imprimer</a>', 
+            url
+        )
+    bouton_imprimer.short_description = 'Impression'
