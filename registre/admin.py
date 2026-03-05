1a20ba1f2b4c206f1114790aba6d92198aa062da
Mggmv <mgbehe14@gmail.com>
2026-03-05 13:03:18 +0530

    Update admin.py

--- registre/admin.py
+++ registre/admin.py
@@ -5,70 +5,32 @@ from .models import Structure, ActeNaissance
 
 @admin.register(Structure)
 class StructureAdmin(admin.ModelAdmin):
-    # Affichage des colonnes dans la liste des structures
     list_display = ('prefecture', 'sous_prefecture', 'nom_centre')
-    search_fields = ('prefecture', 'sous_prefecture', 'nom_centre')
 
 @admin.register(ActeNaissance)
 class ActeNaissanceAdmin(admin.ModelAdmin):
-    # Configuration des colonnes du tableau de bord
-    # 'action_imprimer' affiche le bouton vert à la place du texte d'erreur
-    list_display = (
-        'numero_acte_complet', 
-        'nom_enfant', 
-        'prenoms_enfant', 
-        'get_prefecture', 
-        'get_sous_prefecture', 
-        'action_imprimer'
-    )
+    list_display = ('numero_acte_complet', 'nom_enfant', 'prenoms_enfant', 'action_imprimer')
+    list_filter = ('structure__prefecture', 'annee_registre')
     
-    list_filter = ('structure__prefecture', 'annee_registre', 'date_declaration')
-    search_fields = ('nom_enfant', 'numero_registre')
-    list_select_related = ('structure',)
-
-    # Récupération de la Préfecture depuis le modèle Structure
-    def get_prefecture(self, obj):
-        return obj.structure.prefecture
-    get_prefecture.short_description = 'PRÉFECTURE'
-
-    # Récupération de la Sous-Préfecture depuis le modèle Structure
-    def get_sous_prefecture(self, obj):
-        return obj.structure.sous_prefecture
-    get_sous_prefecture.short_description = 'SOUS-PRÉFECTURE'
-
-    # Création du bouton Imprimer vert
     def action_imprimer(self, obj):
         try:
-            # Utilisation du nom 'voir_extrait' défini dans votre fichier url.txt
-            url = reverse('voir_extrait', kwargs={'pk': obj.pk})
+            [span_4](start_span)url = reverse('voir_extrait', kwargs={'pk': obj.pk}) #[span_4](end_span)
             return format_html(
                 '<a class="button" href="{}" target="_blank" '
                 'style="background-color: #28a745; color: white; padding: 5px 12px; '
-                'border-radius: 4px; text-decoration: none; font-weight: bold; '
-                'display: inline-block; min-width: 80px; text-align: center;">'
-                'Imprimer</a>', 
-                url
-            )
-        except Exception:
-            # Si l'URL n'est pas trouvée, on garde un message discret
-            return format_html('<span style="color: #999;">Lien indisponible</span>')
-    
+                'border-radius: 4px; text-decoration: none; font-weight: bold;">'
+                'Imprimer</a>', url)
+        except:
+            return "Lien indisponible"
     action_imprimer.short_description = 'IMPRESSION'
 
-    # Organisation des champs dans le formulaire de saisie
     fieldsets = (
-        ('Enregistrement', {
-            'fields': ('structure', 'numero_registre', 'annee_registre', 'date_declaration')
-        }),
-        ('Identité de l\'enfant', {
-            'fields': ('nom_enfant', 'prenoms_enfant', 'date_naissance', 'heure_naissance', 'lieu_naissance')
-        }),
-        ('Filiation', {
-            'fields': ('nom_pere', 'nationalite_pere', 'nom_mere', 'nationalite_mere')
-        }),
-        ('Mentions Marginales', {
-            'classes': ('collapse',), # Le volet est réduit par défaut
-            'fields': ('date_mariage', 'conjoint_mariage', 'dissolution_mariage', 'date_deces', 'lieu_deces'),
-            'description': 'Remplir uniquement en cas de mise à jour de l\'acte.'
+        ('1. Registre', {'fields': ('structure', 'numero_registre', 'annee_registre', 'date_declaration')}),
+        ('2. L\'Enfant', {'fields': ('nom_enfant', 'prenoms_enfant', 'date_naissance', 'heure_naissance', 'lieu_naissance')}),
+        ('3. Filiation', {'fields': (('nom_pere', 'nationalite_pere'), ('nom_mere', 'nationalite_mere'))}),
+        ('4. Transcription', {'fields': ('transcription_justice',)}),
+        ('5. Mentions Marginales', {
+            'classes': ('collapse',), 
+            'fields': ('date_mariage', 'conjoint_mariage', 'dissolution_mariage', 'date_deces', 'lieu_deces')
         }),
     )
