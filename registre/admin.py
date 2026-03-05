3244e9723c69f4d1836f26839d4a179d5e17e32f
Mggmv <mgbehe14@gmail.com>
2026-03-05 10:06:06 +0530

    Update admin.py

--- registre/admin.py
+++ registre/admin.py
@@ -1,56 +1,24 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
 from .models import Structure, ActeNaissance
 
 @admin.register(Structure)
 class StructureAdmin(admin.ModelAdmin):
-    """Configuration pour la gestion des structures (Préfecture, Sous-Préfecture, Centre)"""
-    # Affichage en colonnes dans la liste
     list_display = ('prefecture', 'sous_prefecture', 'nom_centre')
-    
-    # Filtre latéral pour regrouper rapidement par Préfecture
-    list_filter = ('prefecture', 'sous_prefecture')
-    
-    # Barre de recherche
-    search_fields = ('prefecture', 'sous_prefecture', 'nom_centre')
-    
-    ordering = ('prefecture', 'sous_prefecture')
+    list_filter = ('prefecture',)
+    search_fields = ('nom_centre', 'prefecture')
 
 @admin.register(ActeNaissance)
 class ActeNaissanceAdmin(admin.ModelAdmin):
-    """Configuration pour la gestion des actes avec toutes les mentions marginales"""
+    # Ajout de 'action_imprimer' dans la liste pour afficher le bouton vert
+    list_display = ('numero_acte_complet', 'nom_enfant', 'prenoms_enfant', 'get_prefecture', 'get_sous_prefecture', 'action_imprimer')
     
-    # 1. Optimisation de la performance (évite la lenteur)
-    list_select_related = ('structure',) 
-    list_per_page = 20 # Paging pour éviter de charger trop de données d'un coup
-    
-    # 2. Affichage de la liste des actes
-    list_display = ('numero_acte_complet', 'nom_enfant', 'prenoms_enfant', 'get_prefecture', 'get_sous_prefecture', 'date_declaration')
-    
-    # 3. Filtres pour le regroupement et les statistiques
-    # On utilise 'structure__prefecture' pour filtrer les actes par leur préfecture rattachée
-    list_filter = ('structure__prefecture', 'structure__sous_prefecture', 'annee_registre', 'date_declaration')
-    
-    # 4. Recherche par nom ou numéro d'acte
-    search_fields = ('nom_enfant', 'prenoms_enfant', 'numero_registre')
+    # Optimisation pour éviter les erreurs de chargement (OperationalError)
+    list_select_related = ('structure',)
+    list_filter = ('structure__prefecture', 'structure__sous_prefecture', 'annee_registre')
+    search_fields = ('nom_enfant', 'numero_registre')
 
-    # 5. Organisation du formulaire de saisie (inclut les mentions marginales)
-    fieldsets = (
-        ('Informations Administratives', {
-            'fields': ('structure', 'numero_registre', 'annee_registre', 'date_declaration')
-        }),
-        ('Identité de l’Enfant', {
-            'fields': ('nom_enfant', 'prenoms_enfant', 'date_naissance', 'heure_naissance', 'lieu_naissance')
-        }),
-        ('Filiation', {
-            'fields': (('nom_pere', 'nationalite_pere'), ('nom_mere', 'nationalite_mere'))
-        }),
-        ('Mentions Marginales (Éventuellement)', {
-            'description': "Toutes les mentions définies dans le modèle sont affichées ici",
-            'fields': ('transcription_justice', 'date_mariage', 'conjoint_mariage', 'dissolution_mariage', 'date_deces', 'lieu_deces')
-        }),
-    )
-
-    # Méthodes pour afficher les informations de la structure dans la liste des actes
     def get_prefecture(self, obj):
         return obj.structure.prefecture
     get_prefecture.short_description = 'Préfecture'
@@ -58,3 +26,30 @@ class ActeNaissanceAdmin(admin.ModelAdmin):
     def get_sous_prefecture(self, obj):
         return obj.structure.sous_prefecture
     get_sous_prefecture.short_description = 'Sous-Préfecture'
+
+    # Fonction pour générer le bouton d'impression sur chaque ligne
+    def action_imprimer(self, obj):
+        # On tente de récupérer l'URL de l'extrait. 
+        # Note : Vérifiez que 'extrait_naissance' est bien le nom dans votre urls.py
+        try:
+            url = reverse('extrait_naissance', args=[obj.id])
+            return format_html(
+                '<a class="button" href="{}" target="_blank" '
+                'style="background-color: #28a745; color: white; padding: 5px 12px; '
+                'border-radius: 4px; text-decoration: none; font-weight: bold;">'
+                'Imprimer</a>', 
+                url
+            )
+        except:
+            return "Lien indisponible"
+    
+    action_imprimer.short_description = 'Impression'
+
+    # Organisation du formulaire de saisie avec les mentions marginales
+    fieldsets = (
+        ('Administration', {'fields': ('structure', 'numero_registre', 'annee_registre', 'date_declaration')}),
+        ('Enfant', {'fields': ('nom_enfant', 'prenoms_enfant', 'date_naissance', 'heure_naissance', 'lieu_naissance')}),
+        ('Mentions Marginales', {
+            'fields': ('date_mariage', 'conjoint_mariage', 'dissolution_mariage', 'date_deces', 'lieu_deces')
+        }),
+    )
