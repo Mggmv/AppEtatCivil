5c427411107cef149599694609d3262f9c165422
Mggmv <mgbehe14@gmail.com>
2026-03-05 13:13:41 +0530

    Update admin.py

--- registre/admin.py
+++ registre/admin.py
@@ -10,20 +10,22 @@ class StructureAdmin(admin.ModelAdmin):
 @admin.register(ActeNaissance)
 class ActeNaissanceAdmin(admin.ModelAdmin):
     list_display = ('numero_acte_complet', 'nom_enfant', 'prenoms_enfant', 'action_imprimer')
-    list_filter = ('structure__prefecture', 'annee_registre')
     
     def action_imprimer(self, obj):
         try:
-            [span_4](start_span)url = reverse('voir_extrait', kwargs={'pk': obj.pk}) #[span_4](end_span)
+            # [span_0](start_span)Utilise 'voir_extrait' défini dans url.txt[span_0](end_span)
+            url = reverse('voir_extrait', kwargs={'pk': obj.pk})
             return format_html(
                 '<a class="button" href="{}" target="_blank" '
                 'style="background-color: #28a745; color: white; padding: 5px 12px; '
-                'border-radius: 4px; text-decoration: none; font-weight: bold;">'
-                'Imprimer</a>', url)
+                'border-radius: 4px; text-decoration: none; font-weight: bold;">Imprimer</a>', 
+                url
+            )
         except:
             return "Lien indisponible"
     action_imprimer.short_description = 'IMPRESSION'
 
+    # Organisation de TOUS les champs (mentions incluses)
     fieldsets = (
         ('1. Registre', {'fields': ('structure', 'numero_registre', 'annee_registre', 'date_declaration')}),
         ('2. L\'Enfant', {'fields': ('nom_enfant', 'prenoms_enfant', 'date_naissance', 'heure_naissance', 'lieu_naissance')}),
